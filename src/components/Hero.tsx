f796d2de3e2e2adee2d4dd9b5a7c337a9840b06e
Gourav Mishra <gouravkrishna23@gmail.com>
2025-06-18 13:06:17 +0530

    Update Hero.tsx (Resume Update)

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -72,7 +72,7 @@ const Hero = () => {
               size="lg" 
               className="rounded-full bg-gradient-to-r from-blue-500 to-blue-300 text-white hover:opacity-90"
             >
-              <a href="https://drive.google.com/file/d/1H7YZSDgqefc35ludzJsE7SSYhFad6_Vx/view?usp=sharing" download>
+              <a href="https://drive.google.com/file/d/1apehS-Y-GlqkJzdcGlwS5HpIq912cNUE/view?usp=sharing" download>
                 <Download className="mr-2 h-5 w-5" />
                 Resume
               </a>
